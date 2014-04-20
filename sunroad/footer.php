c926c98e821ebdd9d014bc00a4ad43d53c366e4d
Bryce Benton <brycebenton@gmail.com>
2014-04-20 17:04:51 -0500

    initial import

---
+++ sunroad/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Sunspot
+ * @since Sunspot 1.0
+ */
+?>
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					Hosted by <a href="http://www.dreamhost.com/r.cgi?100766" title="Full Disclosure: This is an affiliate link, which means I get paid a tiny bit if you follow this link and then sign up for service with DreamHost. I've been a customer consistently since 2005. Alternatively, you're welcome to send me money directly ;) --bryce">DreamHost</a>
+					
+				</div><!-- .site-info -->
+			</footer><!-- .site-footer .site-footer -->
+		</div><!-- #main -->
+
+	</div><!-- #wrapper .wrap -->
+</div><!-- #page .hfeed .site -->
+<div class="sunstrip-small"></div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
